f7894bb5c34e4879fbd2aa8cacecbe75d1456834
romanrubanyk <roman.rubanyk@onix-systems.com>
2024-12-19 02:10:24 +0200

    feat/added homework-2-1 and some setups

---
+++ src/homeworks/homework-2-1/interfaces.ts
@@ -0,0 +1,13 @@
+export interface IContactInfo {
+  email: string;
+  phone: string;
+}
+
+export interface IInfo {
+  firstName: string;
+  lastName: string;
+  birthDay: Date;
+  gender: string;
+  email: string;
+  phone: string;
+}
---
+++ src/homeworks/homework-2-1/types.ts
@@ -0,0 +1,5 @@
+export type TRoles = 'student' | 'teacher';
+
+export type TDisciplines = 'Computer Science' | 'Mathematics' | 'Physics' | 'Biology' | 'Chemistry';
+
+export type TAcademicStatuses = 'active' | 'academic leave' | 'graduated' | 'expelled';
