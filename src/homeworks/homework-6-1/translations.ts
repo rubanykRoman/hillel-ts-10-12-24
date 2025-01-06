6d24c2701284ae4e2f2e7027e209f225aa951600
romanrubanyk <roman.rubanyk@onix-systems.com>
2025-01-06 04:38:38 +0200

    feat/added homework-6-1

---
+++ src/homeworks/homework-6-1/translations.ts
@@ -0,0 +1,35 @@
+type TTranslations = {
+  [languageCode: string]: string | undefined;
+};
+
+const appTranslations: TTranslations = {
+  en: 'Hello!',
+  ua: 'Привіт!',
+  de: 'Hallo!',
+  fr: 'Salut!',
+  es: 'Hola!',
+  br: 'Olá!',
+  cn: undefined,
+};
+
+console.log(appTranslations['ua']); // Привіт!
+console.log(appTranslations['de']); // Hallo!
+// console.log(appTranslations.jp); --- Error: Property 'jp' does not exist on type 'TTranslations'.
+console.log(appTranslations['jp']); // undefined
+
+type TOptionalTranslations = {
+  default?: string;
+};
+
+const extendedTranslations: TTranslations & TOptionalTranslations = {
+  ...appTranslations,
+  default: 'Hello world!',
+};
+
+console.log(extendedTranslations.default); // Hello world!
+console.log(extendedTranslations['default']); // Hello world!
+
+console.log(extendedTranslations['ua']); // Привіт!
+console.log(extendedTranslations['de']); // Hallo!
+// console.log(extendedTranslations.jp); --- Error: Property 'jp' does not exist on type 'TTranslations'.
+console.log(extendedTranslations['jp']); // undefined
