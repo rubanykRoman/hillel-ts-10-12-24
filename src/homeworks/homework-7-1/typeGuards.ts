01ac45f2ccca066a10652165f7445d459c8d6328
romanrubanyk <roman.rubanyk@onix-systems.com>
2025-01-16 23:03:15 +0200

    feat/upd aft rev

--- src/homeworks/homework-7-1/typeGuards.ts
+++ src/homeworks/homework-7-1/typeGuards.ts
@@ -1,36 +1,70 @@
-type TUser = {
-  username: string;
-  password: string;
-};
+interface IUsersMap {
+  user: { username: string; password: string };
+  guest: { sessionId: string };
+  admin: { username: string; password: string; role: 'admin' };
+  externalUser: { oauthToken: string };
+}
 
-type TGuest = {
-  sessionId: string;
-};
+class UsersUtils {
+  private constructor() {}
 
-type TAdmin = TUser & {
-  role: 'admin';
-};
+  public static isUser(user: unknown): user is IUsersMap['user'] {
+    return (
+      UsersUtils.isUserObj(user) &&
+      'username' in user &&
+      typeof user.username === 'string' &&
+      'password' in user &&
+      typeof user.password === 'string'
+    );
+  }
 
-type TExternalUser = {
-  oauthToken: string;
-};
+  public static isGuest(user: unknown): user is IUsersMap['guest'] {
+    return UsersUtils.isUserObj(user) && 'sessionId' in user && typeof user.sessionId === 'string';
+  }
 
-type TPossibleUsersTypes = TUser | TGuest | TAdmin | TExternalUser;
+  public static isAdmin(user: unknown): user is IUsersMap['admin'] {
+    return (
+      UsersUtils.isUserObj(user) &&
+      'username' in user &&
+      typeof user.username === 'string' &&
+      'password' in user &&
+      typeof user.password === 'string' &&
+      'role' in user &&
+      typeof user.role === 'string' &&
+      user.role === 'admin'
+    );
+  }
+  public static isExternalUser(user: unknown): user is IUsersMap['externalUser'] {
+    return (
+      UsersUtils.isUserObj(user) && 'oauthToken' in user && typeof user.oauthToken === 'string'
+    );
+  }
 
-const login = (entity: TPossibleUsersTypes): void => {
-  if ('role' in entity && entity.role === 'admin') {
+  private static isUserObj(user: unknown): user is Record<string, unknown> {
+    return (
+      typeof user === 'object' &&
+      user !== null &&
+      !Array.isArray(user) &&
+      Object.keys(user).length > 0
+    );
+  }
+}
+
+const login = (entity: unknown): void => {
+  if (UsersUtils.isAdmin(entity)) {
     console.log('Authenticating with admin rights');
-  } else if ('username' in entity) {
+  } else if (UsersUtils.isUser(entity)) {
     console.log('Authenticating user with username');
-  } else if ('sessionId' in entity) {
+  } else if (UsersUtils.isGuest(entity)) {
     console.log('Authenticating guest with sessionId');
-  } else if ('oauthToken' in entity) {
+  } else if (UsersUtils.isExternalUser(entity)) {
     console.log('Authenticating external user with oauthToken');
   } else {
     throw new Error('Unknown user type');
   }
 };
 
+//examples
 login({ username: 'user', password: '123' });
 login({ sessionId: '123' });
 login({ role: 'admin', username: 'admin', password: 'admin' });
