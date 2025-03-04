5ea86899400b6b3e7508cbd6521a0ae256a7137d
romanrubanyk <roman.rubanyk@onix-systems.com>
2025-03-05 00:19:53 +0200

    feat/updated after review

--- src/homeworks/homework-18-1/bank_account.ts
+++ src/homeworks/homework-18-1/bank_account.ts
@@ -7,6 +7,7 @@ class BankAccount implements IBankAccount {
   private _balance: number;
   private _owner: Client;
   private readonly _currency: string;
+  private transactionHistory = new TransactionHistory();
 
   public readonly accountNumber = this.generateAccountNumber();
 
@@ -32,14 +33,31 @@ class BankAccount implements IBankAccount {
     this._currency = currency;
   }
 
-  public deposit(amount: number): void {
+  private deposit(amount: number): void {
     this._balance += amount;
+    this.transactionHistory.addTransaction('deposit', amount);
     console.log(`Operation: Deposit. Balance: ${this.balance} ${this.currency}`);
   }
 
-  public withdraw(amount: number): void {
+  private withdraw(amount: number): void {
+    if (this._balance < amount) {
+      throw new Error(`Not enough funds: requested ${amount}, available ${this._balance}`);
+    }
     this._balance -= amount;
+    this.transactionHistory.addTransaction('withdraw', amount);
     console.log(`Operation: Withdraw. Balance: ${this.balance} ${this.currency}`);
+  }
+
+  public processTransaction(type: TransactionType, amount: number): void {
+    if (type === 'deposit') {
+      this.deposit(amount);
+    } else if (type === 'withdraw') {
+      this.withdraw(amount);
+    }
+  }
+
+  public getTransactionHistory(): ReadonlyArray<Transaction> {
+    return this.transactionHistory.transactions;
   }
 
   private generateAccountNumber(): string {
@@ -74,7 +92,7 @@ export class Client {
   }
 }
 
-class Transaction {
+export class Transaction {
   public readonly amount: number;
   public readonly date = Date.now();
   public readonly id: number;
@@ -89,13 +107,15 @@ class Transaction {
 
 class TransactionHistory {
   private readonly _transactions: Transaction[] = [];
+  private transactionIdCounter = 1;
 
   public get transactions(): ReadonlyArray<Transaction> {
     return this._transactions;
   }
 
   public addTransaction(type: TransactionType, amount: number): void {
-    this._transactions.push(new Transaction(type, amount, 42));
+    this._transactions.push(new Transaction(type, amount, this.transactionIdCounter));
+    this.transactionIdCounter++;
   }
 }
 
@@ -133,10 +153,10 @@ class DepositCommand implements Command {
     private amount: number
   ) {}
   execute(): void {
-    this.account.deposit(this.amount);
+    this.account.processTransaction('deposit', this.amount);
   }
   undo(): void {
-    this.account.withdraw(this.amount);
+    this.account.processTransaction('withdraw', this.amount);
   }
 }
 
@@ -146,10 +166,10 @@ class WithdrawCommand implements Command {
     private amount: number
   ) {}
   execute(): void {
-    this.account.withdraw(this.amount);
+    this.account.processTransaction('withdraw', this.amount);
   }
   undo(): void {
-    this.account.deposit(this.amount);
+    this.account.processTransaction('deposit', this.amount);
   }
 }
 
@@ -211,3 +231,9 @@ console.log(
 
 bank.closeAccount(account1);
 console.log("Client's accounts after closing account1:", clientJohn.getAccounts());
+
+console.log('Transaction History for Account1:');
+console.log(account1.getTransactionHistory());
+
+console.log('Transaction History for Account2:');
+console.log(account2.getTransactionHistory());
